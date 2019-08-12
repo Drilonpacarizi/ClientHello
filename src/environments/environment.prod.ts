fdf580a8e8f9b8487fa0cf757101da22eb16b64e
Drilon <drilonpa@gmail.com>
2019-08-12 22:29:34 +0200

    Client Panel

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firbase: {
+    apiKey: "AIzaSyDoIQ8eaK1uAUkYHeSKikrB44Zv8C_psVI",
+    authDomain: "clientpanelprod-51726.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-51726.firebaseio.com",
+    projectId: "clientpanelprod-51726",
+    storageBucket: "clientpanelprod-51726.appspot.com",
+    messagingSenderId: "378764410485",
+    appId: "1:378764410485:web:b832912516c5cadc"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firbase: {
+    apiKey: "AIzaSyDoIQ8eaK1uAUkYHeSKikrB44Zv8C_psVI",
+    authDomain: "clientpanelprod-51726.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-51726.firebaseio.com",
+    projectId: "clientpanelprod-51726",
+    storageBucket: "clientpanelprod-51726.appspot.com",
+    messagingSenderId: "378764410485",
+    appId: "1:378764410485:web:b832912516c5cadc"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
